075d7cdf7e19b2d7adab1e3a3ee855c6d46fd8b0
tomel999 <146027763+tomel999rewtwetwed@users.noreply.github.com>
2024-11-07 20:32:24 +0100

    Create linux-install.sh

---
+++ linux-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo su
+sudo apt update
+sudo apt install -y git nodejs npm python3 pip
+git clone 
+cd
+npm install
+npm install express
+npm install discord-rpc
+pip install spotdl
