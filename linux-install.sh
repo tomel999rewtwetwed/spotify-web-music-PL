595bcc2f50c21001f42d2fa81cd70efa9d2ce906
tomel999 <146027763+tomel999rewtwetwed@users.noreply.github.com>
2024-11-07 20:32:46 +0100

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -3,8 +3,8 @@
 sudo su
 sudo apt update
 sudo apt install -y git nodejs npm python3 pip
-git clone 
-cd
+git clone https://github.com/tomel999rewtwetwed/spotify-web-music-PL.git
+cd spotify-web-music-PL
 npm install
 npm install express
 npm install discord-rpc
